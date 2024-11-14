2d3b1fc8638e23d685d7824c2068a164bf8253d6
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-11-14 23:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+let colorSelect = document.getElementById("colorSelect");
+let input = document.querySelector("input");
+
+let option = document.querySelectorAll("option");
+
+let color
+
+colorSelect.addEventListener("change", (e) => {
+  color = e.target.value;
+});
+
+input.addEventListener("click", () => {
+  option.forEach((val) => {
+    if (val.innerHTML == color)  {
+        val.remove()
+    }
+  });
+  console.log(color);
+  
+});
